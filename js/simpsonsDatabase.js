1c834c4ca222a06f4a7f57826eb1e7846dc25707
Bitajune <beheshtibita@gmail.com>
2020-03-24 13:11:38 -0700

    simpsons database

---
+++ js/simpsonsDatabase.js
@@ -0,0 +1,52 @@
+let simpson = [
+    {
+        title: 'Barney Gumble',
+        img: './assets/images/barney.png',
+        description: "*Buuuuuuuurp*"
+    },
+    {
+        title: 'Bart Simpsons',
+        img: './assets/images/bart.png',
+        description: "Nothing you say can upset us. We're the MTV generation."
+    },
+    {
+        title: 'Mr. Burns',
+        img: './assets/images/burns.png',
+        description: "Family, religion, friendship. These are the three demons you must slay if you wish to succeed in busniess."
+    },
+    {
+        title: 'Comic Book Guy',
+        img: './assets/images/comic.png',
+        description: "Tell me, how do you feel about 45-year-old virgins who still live with their parents?"
+    },
+    {
+        title: 'Homer Simpson',
+        img: './assets/images/homer.png',
+        description: "Kwyjibo: A big, dumb, balding North American ape with no chin and a short temper."
+    },
+    {
+        title: 'Lisa Simpson',
+        img: './assets/images/lisa.png',
+        description: "I don't get. Straight A's, perfect attendance, bathroom timer. I should be the most popular girl in school."
+    },
+    {
+        title: 'Ned Flanders',
+        img: './assets/images/ned.png',
+        description: "Shut up, Flanders."
+    },
+    {
+        title: 'Patty and Selma',
+        img: './assets/images/patty.png',
+        description: "Just close your eyes and think of MacGyver."
+    },
+    {
+        title: 'Smithers',
+        img: './assets/images/smithers.png',
+        description: "I think that women and seamen don't mix."
+    },
+    {
+        title: 'Milhouse',
+        img: './assets/images/milhouse.png',
+        description: "But my mom says I'm cool."
+    },
+];
